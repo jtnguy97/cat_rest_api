b77fa0c182e7ca8de7bbe62b1f7d34b155a354c3
jtnguy29 <jtnguy29@uno.edu>
2023-07-16 21:04:58 -0500

    fix invalid case

--- scripts/state.js
+++ scripts/state.js
@@ -4,9 +4,20 @@ const searchBtn = document.querySelector(".search button");
 
         
     async function checkState(state){
-        const response = await fetch(apiUrlState + state)
-        var data = await response.json();
-        console.log(data);
+        const response = await fetch(apiUrlState + state);
+
+        //if invalid state name, display error message. do not display anything, set to none
+        //else search state
+
+        if(response.status == 404){
+            document.querySelector(".error").style.display = "block";
+            document.querySelector(".state_details").style.display = "none";
+        }
+
+        //else if valid state name, do not display error message. display state details
+        else{
+            var data = await response.json();
+            console.log(data);
 
             document.querySelector(".state").innerHTML = data.state;
             document.querySelector(".active_cases").innerHTML = "active cases: " + data.active;
@@ -15,10 +26,15 @@ const searchBtn = document.querySelector(".search button");
             document.querySelector(".total_cases").innerHTML = "total cases: " + data.cases;
             document.querySelector(".total_deaths").innerHTML = "total deaths: " + data.deaths;
             document.querySelector(".population").innerHTML = "population: " + data.population;
+            document.querySelector(".error").style.display = "none";
+            document.querySelector(".state_details").style.display = "block";
+           
         }
+    }
+
 
     searchBtn.addEventListener("click", () => {
-    checkState(searchBox.value);
+        checkState(searchBox.value);
     })
 
   
